64537704b9d3b44af38bc656dd78549d26acb09d
Craig A <craigadam1987@gmail.com>
2024-07-06 19:00:50 +0100

    styling for offer page complete

--- Book&Board/pages/offer.php
+++ Book&Board/pages/offer.php
@@ -40,52 +40,63 @@ if ($offer) {
 <body>
 
     <main>
-        <h1><?php echo $offer->location; ?></h1>
-        <h2>Star Rating: <?php echo $offer->starRating; ?></h2>
-        <p>Dates: <?php echo $offer->dates; ?></p>
-        <p>Description: <?php echo $offer->description; ?></p>
+        <section id="offer">
+            <h4 class="card-title"><?php echo $offer->location; ?></h4>
+            <h5 class="star-rating"><span> <?php echo $offer->starRating; ?></span></h5>
+            <h5 class="travel-dates">Dates: <?php echo $offer->dates; ?></h5>
+            <p class="travel-description"><?php echo $offer->description; ?></p>
+            <p class="offer-price"><?php echo ($offer->price) ?></p>
+            <div class="content-container">
+                <div class="slideshow-container">
+                    <!-- create a carousel to display all images about the destination-->
+                    <!-- Loop though each image, on the offers image array -->
+                    <?php foreach ($offer->images as $index => $image) { ?>
+                    <div class="mySlides fade">
+                        <!-- The index is pulled out of the offer object so that alt text is different for each image -->
+                        <img class="holiday-image" src="<?php echo $image; ?>"
+                            alt="<?php echo $offer->location . ' image-' . ($index + 1); ?>">
+                        <!-- Trigger javascript to control clicking the carousel arrow buttons -->
+                        <div class="arrow-container">
+                            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                            <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                        </div>
+                    </div>
+                    <?php } ?>
 
-        <div class="slideshow-container">
-            <!-- create a carousel to display all images about the destination-->
-            <!-- Loop though each image, on the offers image array -->
-            <?php foreach ($offer->images as $index => $image) { ?>
-            <div class="mySlides fade">
-                <!-- The index is pulled out of the offer object so that alt text is different for each image -->
-                <img class="holiday-image" src="<?php echo $image; ?>"
-                    alt="<?php echo $offer->location . ' image-' . ($index + 1); ?>">
+                    <div class="dot-container">
+                        <?php foreach ($offer->images as $index => $image) { ?>
+                        <!-- A clickable circle marker is create for each image -->
+                        <!-- This allows users to click the marker to jump to that image -->
+                        <!-- Index is used to pass the image position number to a javascript function -->
+                        <span class="dot" onclick="currentSlide(<?php echo $index + 1; ?>)"></span>
+                        <?php } ?>
+                    </div>
+                </div>
+
             </div>
-            <?php } ?>
-            <!-- Trigger javascript to control clicking the carousel arrow buttons -->
-            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-            <a class="next" onclick="plusSlides(1)">&#10095;</a>
-        </div>
-        <br>
-        <div style="text-align:center">
-            <?php foreach ($offer->images as $index => $image) { ?>
-            <!-- A clickable circle marker is create for each image -->
-            <!-- This allows users to click the marker to jump to that image -->
-            <!-- Index is used to pass the image position number to a javascript function -->
-            <span class="dot" onclick="currentSlide(<?php echo $index + 1; ?>)"></span>
-            <?php } ?>
-        </div>
+            <section class="additional-info">
+                <div class="activities">
+                    <h5>Activities:</h5>
+                    <ul>
+                        <!-- Each activity is listed by looping through the offers activities array -->
+                        <?php foreach ($offer->activities as $activity) { ?>
+                        <li><?php echo $activity; ?></li>
+                        <?php } ?>
+                    </ul>
+                </div>
+                <div class="facilities">
+                    <h5>Facilities:</h5>
+                    <ul>
+                        <!-- Each facility is listed by looping through the offers facilities array -->
+                        <?php foreach ($offer->facilities as $facility) { ?>
+                        <li><?php echo $facility; ?></li>
+                        <?php } ?>
+                    </ul>
+                </div>
+            </section>
 
-        <p>Price: <?php echo $offer->price; ?></p>
 
-        <h3>Activities:</h3>
-        <ul>
-            <!-- Each activity is listed by looping through the offers activities array -->
-            <?php foreach ($offer->activities as $activity) { ?>
-            <li><?php echo $activity; ?></li>
-            <?php } ?>
-        </ul>
-
-        <h3>Facilities:</h3>
-        <ul>
-            <!-- Each facility is listed by looping through the offers facilities array -->
-            <?php foreach ($offer->facilities as $facility) { ?>
-            <li><?php echo $facility; ?></li>
-            <?php } ?>
-        </ul>
+        </section>
     </main>
     <script src="../js/offer.js"></script>
 </body>
@@ -97,4 +108,6 @@ if ($offer) {
     header('Location: error.php');
     exit;
 }
+
+include '../partials/footer.php';
 ?>
