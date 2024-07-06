39e13320241fbc4068c6e3d28422761e503759b7
Craig A <craigadam1987@gmail.com>
2024-07-06 19:34:04 +0100

    Styled error page

--- Book&Board/pages/error.php
+++ Book&Board/pages/error.php
@@ -1,9 +1,13 @@
 <?php include '../partials/header.php'; ?>
 <?php include '../partials/navbar.php'; ?>
-
+<link rel="stylesheet" href="../css/error.css">
 <main>
-    <h1>404 Error</h1>
-    <p>Sorry, the resource could not be found</p>
-</main>
+    <section id="error">
+        <h1>404 Error</h1>
+        <p>Sorry, the resource could not be found</p>
+        <button id="home" type="submit">Home</button>
+    </section>
 
+</main>
+<script src="../js/error.js"></script>
 <?php include '../partials/footer.php'; ?>
